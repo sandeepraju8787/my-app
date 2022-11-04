2fe381b71cb9a905a5d3f519878604984283c732
Sandeep Raju <sandeep.raju@oracle.com>
2022-11-04 19:06:39 +0530

    calculator file added.

--- src/List.jsx
+++ src/List.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { add, sub, mul } from "./Calculator";
 
 function List() {
   return (
     <ol>
-      <li>Music</li>
-      <li>Tech</li>
-      <li>Sports</li>
+      <li>Music + {add(1, 2)}</li>
+      <li>Tech {mul(3, 3)}</li>
+      <li>Sports {sub(9, 4)}</li>
       <li>Business</li>
       <li>Please subscribe</li>
     </ol>
