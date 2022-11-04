8c4d85cb531f4f0274254c9a4446d917c518d2cc
Sandeep Raju <sandeep.raju@oracle.com>
2022-11-04 18:55:30 +0530

    changes1

---
+++ src/List.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function List() {
+  return (
+    <ol>
+      <li>Music</li>
+      <li>Tech</li>
+      <li>Sports</li>
+      <li>Business</li>
+      <li>Please subscribe</li>
+    </ol>
+  );
+}
+
+export default List;
